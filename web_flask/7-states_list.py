0988dad5fc4176e1863ffd9505e73d3f08944580
RicardoDanta <uruguaydenos@gmail.com>
2022-12-04 12:47:46 -0800

    Change some things!

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_appcontext():
+    """Method to handle app.teardown_appcontext"""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states():
+    """Function that display a HTML page"""
+    return render_template("7-states_list.html", states=storage.all("State"))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
