6c1fcea99edfbe78e11f547add6d04467caabf57
RicardoDanta <uruguaydenos@gmail.com>
2022-12-04 12:54:51 -0800

    Delete Self!

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.teardown_appcontext
 def teardown_appcontext(self):
     """Method to handle app.teardown_appcontext"""
-    self.storage.close()
+    storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
